39b73c01e5cb046830b4a55c22ddfbfa0ccbafd6
michele131 <michele.corrado@marconirovereto.it>
2019-11-05 13:35:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gestioneagenda
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gestioneagenda)
---
+++ src/app/contatto.components/contatto.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Contatto } from './contatto.model.ts';
+
+@Component({
+  selector: 'app-contatto',
+  templateUrl: './contatto.component.html',
+  styleUrls: ['./contatto.component.css']
+})
+
+export class ContattoComponent {
+  contatto: Contatto = new Contatto('Michele', 'Corrado', 'michele.corrado@marconirovereto.it', '+393402747394', 5);
+
+
+
+}
